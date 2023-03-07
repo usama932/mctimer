d5301e9f7069455fd75c1d53c3c827bb15165def
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-03-07 15:37:43 +0500

    Update AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Models\User;
-use App\Traits\ApiResponser;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +17,7 @@ use Illuminate\Validation\Rules\Password as RulesPassword;
 
 class AuthController extends Controller
 {
-    use ApiResponser;
+  
 
     public function register(Request $request)
     {
