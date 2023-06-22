c276da8767bb931028ea8e0438a0701b5cab556e
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-06-22 02:56:49 -0700

    Update AuthController.php

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -231,7 +231,7 @@ class AuthController extends ApiController
             [$id, $token] = explode('|', $post_data['token'], 2);
             $token_data = DB::table('personal_access_tokens')->where('token', hash('sha256', $token))->first();
             $user_id = $token_data->tokenable_id;
-            dd($user);
+            dd($user_id);
             $res = [
 
                 'message' => 'Successfully deleted',
